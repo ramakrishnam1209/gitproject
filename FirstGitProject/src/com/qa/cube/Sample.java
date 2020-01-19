328f2b56bf05a596d71fd7cc1ba3e44a168d3b77
basava <basavapunnaiah81@gmail.com>
2020-01-19 15:29:51 +0530

    array created and iterated

--- FirstGitProject/src/com/qa/cube/Sample.java
+++ FirstGitProject/src/com/qa/cube/Sample.java
@@ -4,7 +4,14 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+		String[] sub= {"Java","Spring","Hibernate"};
+		
+		for(int i=0;i<sub.length;i++) {
+			System.out.println("Hello "+sub[i]);
+		}
+		
 	}
 
 }
