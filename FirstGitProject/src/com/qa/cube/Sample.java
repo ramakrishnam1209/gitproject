c8b3b9c5ed96e2d673e469ba2ef5bc0203db06fd
basava 1 <basavapunnaiah811@gmail.com>
2020-01-19 15:35:13 +0530

    sysout added by second developer

--- FirstGitProject/src/com/qa/cube/Sample.java
+++ FirstGitProject/src/com/qa/cube/Sample.java
@@ -3,8 +3,10 @@ package com.qa.cube;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
           System.out.println("Hello World!");
+          
+          System.out.println(" 19th January 2020 ");
+          
 	}
 
 }
